6c05fa01840874943fe37f54e1bd2a5de92bce94
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-31 09:50:29 -0700

    Add Stylist class with properties and HashSet for Clients.

---
+++ Salon/Models/Stylist.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace EauSalon
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    [DisplayName("Stylist's Name:")]
+    public string Name { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
